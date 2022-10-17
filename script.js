f06e752b2ba41923531000855f88b0bc9768e78b
HAU165 <20521301@gm.uit.edu.vn>
2022-10-17 17:10:30 +0700

    third commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const numbers = document.getElementsByClassName('button');
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+
+for (let number of numbers) {
+    number.addEventListener('click', function () {
+        input.innerHTML += this.value;
+    });
+}
+
+// document.addEventListener('click', equal)
+function equal() {
+    let numDisplay = input.innerHTML;
+    let output = eval(numDisplay);
+    result.innerHTML = output;
+}
+function clean() {
+    input.innerHTML = " ";
+    result.innerHTML = "0";
+}
+
+function undo() {
+    let numDisplay = input.innerHTML;
+    input.innerHTML = numDisplay.substring(0, numDisplay.length - 1);
+}
